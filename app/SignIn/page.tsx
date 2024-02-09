1ea74602b0a7d924a80e80140f3d97ed088b2dd9
MWassim1 <mussardwassim92@gmail.com>
2024-02-09 15:00:23 +0100

    add SignIn

---
+++ app/SignIn/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import {useState} from 'react'
+const LoginForm = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleUsernameChange = (e: any) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e: any) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="login-form">
+      <h2>Connexion</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Nom d'utilisateur:</label>
+          <input
+            type="text"
+            value={username}
+            onInput={handleUsernameChange}
+            placeholder="Entrez votre nom d'utilisateur"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Mot de passe:</label>
+          <input
+            type="password"
+            value={password}
+            onInput={handlePasswordChange}
+            placeholder="Entrez votre mot de passe"
+            required
+          />
+        </div>
+        <button type="submit">Se connecter</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
