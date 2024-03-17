f57f4581a462d0a76041a78b6f2df14ed063052a
Mussard <mussardwassim92@gmail.com>
2024-03-17 20:40:55 +0100

    maj de la sidebar + début logout : todo delete cookies

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -1,9 +1,9 @@
 "use client"
 import classNames from "classnames";
 import Link from "next/link";
-import React, { useState, useMemo } from "react";
-import {BsArrowsCollapse} from "react-icons/bs"
-import {router} from "next/client";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { BsArrowsCollapse } from "react-icons/bs";
 
 
 const menuItems = [
@@ -14,92 +14,56 @@ const menuItems = [
 
 export default function Sidebar() {
     const [toggle, setToggle] = useState(false);
-    const [isCollapsible, setIsCollapsible] = useState(false);
-    const handleSidebarToggle = () => setToggle(!toggle);
+    const router = useRouter();
 
-    // const activeMenu = useMemo(
-    //     () => menuItems.find((menu) => menu.link === router.pathname),
-    //     [router.pathname]
-    // );
+    const handleSidebarToggle = () => {
+        setToggle(!toggle);
+    };
 
-
-    const classes = classNames(
-        "h-screen overflow-hidden px-4 sm:px-6 lg:px-8 bg-amber-50 justify-between flex-col w-80 flex text-black",
-        {
-            ["w-80"]: !toggle,
-            ["w-20"]: toggle
-        }
-    )
-
-    const collapseIcons = classNames(
-        "p-4 rounded bg-light-lighter absolute right-0 w-80",
-        {
-            "rotate-180": toggle
-        }
-    )
-    const test = "test"
+    const handleLogout = () => {
+        //todo delete cookie
+        router.push("/login");
+    };
 
     const getNavItemClasses = () => {
         return classNames(
-            "flex items-center cursor-pointer hover:bg-light-lighter rounded w-full overflow-hidden whitespace-nowrap",
-            {
-                ["bg-light-lighter"]: /*activeMenu.id === menu.id*/ true,
-            }
+            "flex items-center cursor-pointer hover:bg-light-lighter rounded w-full overflow-hidden whitespace-nowrap"
         );
-    };
-
-    const onMouseOver = () => {
-        setIsCollapsible(!isCollapsible);
     };
 
     return (
         <div
-            className={classes}
-            onMouseEnter={onMouseOver}
-            onMouseLeave={onMouseOver}
-            style={{transition: "width 300ms cubic-bezier(0.2, 0, 0, 1) 0s"}}
+            className={classNames(
+                "h-screen overflow-hidden px-4 sm:px-6 lg:px-8 bg-amber-50 justify-between flex-col w-80 flex text-black transition-all duration-300",
+                {
+                    "w-80": !toggle,
+                    "w-0": toggle,
+                }
+            )}
         >
             <div className="flex flex-col">
                 <div className="flex items-center justify-between relative">
                     <div className="flex items-center pl-1 gap-4">
-                        {/* Logo Icon */}
-                        <span
-                            className={classNames("mt-2 text-lg font-medium text-text", {
-                                hidden: toggle,
-                            })}
-                        >
-              Not a RaaS ://
-            </span>
+                        <span className={classNames("mt-2 text-lg font-medium text-text", { hidden: toggle })}>
+                            Not a RaaS ://
+                        </span>
                     </div>
-                    {isCollapsible && (
-                        <button
-                            className={collapseIcons}
-                            onClick={handleSidebarToggle}
-                        >
-                            {/* Icone (collapse truc muche) */}
-                        </button>
-                    )}
-                    <BsArrowsCollapse onClick={handleSidebarToggle} />
-
+                    <button className="p-4 rounded bg-light-lighter absolute right-0" onClick={handleSidebarToggle}>
+                        <BsArrowsCollapse />
+                    </button>
                 </div>
                 <div className="flex flex-col items-start mt-24">
                     {menuItems.map(({ icon: Icon, ...menu }) => {
                         const classes = getNavItemClasses();
                         return (
-                            <div className={classes}>
-                                <Link legacyBehavior={true} href={menu.link}>
+                            <div className={classes} key={menu.id}>
+                                <Link  legacyBehavior={true} href={menu.link}>
                                     <a className="flex py-4 px-3 items-center w-full h-full">
                                         <div style={{ width: "2.5rem" }}>
                                             <Icon />
                                         </div>
                                         {!toggle && (
-                                            <span
-                                                className={classNames(
-                                                    "text-md font-medium text-text-light"
-                                                )}
-                                            >
-                        {menu.label}
-                      </span>
+                                            <span className="text-md font-medium text-text-light">{menu.label}</span>
                                         )}
                                     </a>
                                 </Link>
@@ -109,17 +73,14 @@ export default function Sidebar() {
                 </div>
             </div>
 
-            {/*<div className={`/!*${getNavItemClasses({})}*!/ px-3 py-4`}>*/}
-            <div className={`${test} px-3 py-4`}>
+            <div className="px-3 py-4">
                 <div style={{ width: "2.5rem" }}>
-                    {/* LogoutIcon */}
+                    {/* Placeholder for logout icon */}
                 </div>
                 {!toggle && (
-                    <span className={classNames("text-md font-medium text-text-light")}>
-            Logout
-          </span>
+                    <button className="text-md font-medium text-text-light" onClick={handleLogout}>Logout</button>
                 )}
             </div>
         </div>
     );
-};
+}
