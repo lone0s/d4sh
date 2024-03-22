3ac924fd3a994970b6b678768992553552a95548
Mussard <mussardwassim92@gmail.com>
2024-03-22 09:51:38 +0100

    maj nom du cookie

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,3 @@
-export const COOKIE_NAME = "OurSiteJWT"
+export const COOKIE_NAME = "d4ashJWT"
 export const MAX_AGE = 60* 60 * 24 * 30 // 30 jours
 export const SECRET = process.env.JWT_SECRET || "";
