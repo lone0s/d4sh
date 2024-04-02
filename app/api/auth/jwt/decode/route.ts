696435f95f962fc804c0ed04c2b53d3fd7322e12
Mussard <mussardwassim92@gmail.com>
2024-04-02 15:44:11 +0200

    maj des graphes

--- app/api/auth/jwt/decode/route.ts
+++ app/api/auth/jwt/decode/route.ts
@@ -1,10 +1,13 @@
 import {verify, decode} from "jsonwebtoken";
 import {NextApiRequest} from "next";
 import {NextResponse} from "next/server";
+import { cookies } from "next/headers";
 import {COOKIE_NAME} from "@/constants";
 
 export async function GET(req: NextApiRequest) {
-
+    const cookieStore = cookies();
+    const token = cookieStore.get(COOKIE_NAME);
+    console.log(token)
     const jwtCookie = document.cookie
         .split("; ")
         .find((row) => row.startsWith("d4ashJWT="))
